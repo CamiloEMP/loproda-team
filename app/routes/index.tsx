b1477f9c9bc7c0708d15bc35503d97b974a59626
Fernando Ticona <thefersh24@gmail.com>
2022-05-08 10:46:00 -0400

    index header

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,51 @@
-import { Button } from 'flowbite-react'
+import { useEffect, useState } from 'react'
+import { Logo } from '~/components/logo'
 import { Navbar } from '~/components/navbar'
-import { useAppContext } from '~/provider/app'
+
+function HeaderTitle(setText: (text: string) => void, texto: string, n: () => void){
+  var i=0,
+  timer = setInterval(function() {
+    if (i < texto.length ) {
+      setText(texto.substr(0,i++) + "|");
+    } else {
+      clearInterval(timer);
+      setText(texto);
+        setTimeout(n, 2600);
+    }
+  }, 150);
+ };
 
 export default function Index() {
-  const { setTheme, theme } = useAppContext()
+  const [hText, setHText] = useState('')
+  const [hIndex, setHIndex] = useState(0)
+
+  useEffect(() => {
+    const headerText = [
+      'tu equipo',
+      'tu proyecto',
+      'tu organizacion'
+    ]
+    HeaderTitle(setHText, headerText[hIndex], () => {
+      setHIndex(hIndex >= 2 ?  0 : hIndex + 1)
+    })
+  }, [hIndex])
+
   return (
     <>
     <Navbar />
     <main>
-      pagina de presentacion
+      <header className="container mx-auto h-96 flex items-center">
+        <div className='w-2/4'>
+          <span>
+            <Logo></Logo>
+          </span>
+          <h1 className="flex flex-col gap-2">
+            <span className="text-2xl dark:text-white">Gestiona equipos para</span>
+            <span className="text-4xl dark:text-white">{hText}</span>
+          </h1>
+        </div>
+        <div className='w-2/4'>imagen de presentacion</div>
+      </header>
     </main>
     </>
   )
