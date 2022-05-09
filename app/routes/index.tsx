45fcf3e5a336f65239683a57c13346074ec23803
Fernando Ticona <thefersh24@gmail.com>
2022-05-09 19:55:03 -0400

    index seo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import type { MetaFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { Navbar } from '~/components/navbar'
 import { IndexPage } from '~/components/page'
@@ -5,6 +6,20 @@ import type { IndexLoaderI } from '~/loader'
 import { indexLoader } from '~/loader'
 
 export const loader = indexLoader
+
+export const meta: MetaFunction = ({ data }) => {
+  if (data.auth) {
+    return {
+      title: `Inicio | ${data.name}`
+    }
+  } else {
+    return {
+      title: 'Loproda team | Haz tu equipo mas eficiente',
+      description:
+        'Ayuda a tu equipo a gestionar sus tareas con un sistema de gestion sensillo.'
+    }
+  }
+}
 
 export default function Index() {
   const { auth, username, name, avatar } = useLoaderData<IndexLoaderI>()
