b5554529ebc7998dff0b43813ad433c9eb2df18f
Fernando Ticona <thefersh24@gmail.com>
2022-05-11 11:44:55 -0400

    room  card preview

--- app/loader/new.ts
+++ app/loader/new.ts
@@ -3,11 +3,9 @@ import { redirect } from '@remix-run/node'
 import { v4 } from 'uuid'
 import { authenticator } from '~/auth.server'
 import { supabaseClient } from '~/supabase.server'
-import { createRoomVideoTwilio } from '~/twilio/video/createRoom'
 import ShortId from 'short-unique-id'
 
-enum VideoRoomStatus {
-  creating, // la sala esta registrada pero no se creo en Twilio
+export enum VideoRoomStatus {
   available, // Sala lista para iniciar la reunion
   inSession, // Sala en sesion
   finalized // La sala termino la reunion para todos
@@ -35,28 +33,16 @@ export const newLoader: LoaderFunction = async ({ request }) => {
   const uid = new ShortId()
   const id = uid()
   const meetdb = await supabaseClient.from('meetroom').insert({
-    active: false,
+    active: true,
     userid: session.user?.id,
     twiliomax: 2, // por el momento
     twilioname: roomName,
-    status: VideoRoomStatus.creating,
+    status: VideoRoomStatus.available,
     name: '',
     description: '',
     shortid: String(id).toLocaleLowerCase()
   })
 
-  // 5 crear una sala de videoconferencias en Twilio
-  if (meetdb.data?.length === 0) return redirect('/')
-  await createRoomVideoTwilio({
-    name: meetdb.data ? meetdb.data[0].twilioname : roomName
-  })
-
-  // 6 Actualizar registro
-  await supabaseClient
-    .from('meetroom')
-    .update({ status: VideoRoomStatus.available })
-    .match({ id: meetdb.data ? meetdb.data[0].id : '' })
-
-  // 7 redireccionar
+  // redireccionar
   return redirect(meetdb.data ? `/meet/${meetdb.data[0].shortid}` : '/')
 }
