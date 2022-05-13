aeec7f741f72e6604043135fc51228a83ca027f8
Fernando Ticona <thefersh24@gmail.com>
2022-05-12 21:52:37 -0400

    get access token

--- app/routes/meet/$slug.tsx
+++ app/routes/meet/$slug.tsx
@@ -1,21 +1,53 @@
-import type { LoaderFunction } from '@remix-run/node'
+import type { ActionFunction, LoaderFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
 import type { meetSlugLoaderI } from '~/loader/meet.slug'
 import { meetSlugLoader } from '~/loader/meet.slug'
-import { MeetPreMeet } from '~/components/page/meet.premeet'
-import { useLoaderData } from '@remix-run/react'
-import { MeetInMetting } from '~/components/page/meet.meeting'
+import { Form, useActionData, useLoaderData, useParams } from '@remix-run/react'
 import { VideoProvider } from '~/provider/video'
-import { MeetPost } from '~/components/page/meet.postmeet'
+import { supabaseClient } from '~/supabase.server'
+import { findOrCreateRoom } from '~/twilio/video/findOrCreate.room'
+import { getToken } from '~/twilio/video/getToken.room'
+import { v4 } from 'uuid'
 
 export const loader: LoaderFunction = meetSlugLoader
 
+export const action: ActionFunction = async ({ request }) => {
+  const f = await request.formData()
+
+  if (!f.has('slug')) return json({ error: 'slug' })
+  const meet = await supabaseClient
+    .from('meetroom')
+    .select('twilioname, twiliomax, userid')
+    .eq('shortid', f.get('slug'))
+
+  if (meet.error || !meet.data) return json({ error: 'db' })
+
+  const roomName = meet.data ? meet.data[0].twilioname : ''
+  await findOrCreateRoom({ name: roomName, type: 'go' })
+
+  const token = getToken({ roomName, identity: v4() })
+  return json({ ...token })
+}
+
 export default function MeetSlug(): JSX.Element {
+  const params = useParams()
+  const action = useActionData()
   const loader = useLoaderData<meetSlugLoaderI>()
+
   return (
     <VideoProvider>
+      <div className="text-white"></div>
+      <Form method="post">
+        <input type="hidden" name="slug" value={params.slug} />
+        <button type="submit">obtener token o unirse</button>
+      </Form>
+      {JSON.stringify(action)}
+      {/*
+
       <MeetPreMeet loader={loader} />
       <MeetInMetting />
       <MeetPost />
+  */}
     </VideoProvider>
   )
 }
